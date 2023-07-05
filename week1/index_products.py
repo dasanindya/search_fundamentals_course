dae50eda0d8e88e420c38f436c6cf7976e177a8a
Anindya Das <dasanindya15@gmail.com>
2023-07-05 16:02:50 +0000

    index_products update

--- week1/index_products.py
+++ week1/index_products.py
@@ -85,7 +85,15 @@ def get_opensearch():
     port = 9200
     auth = ('admin', 'admin')
     #### Step 2.a: Create a connection to OpenSearch
-    client = None
+    client = OpenSearch(
+        hosts=[{'host': host, 'port': port}],
+        http_compress=True,  # enables gzip compression for request bodies
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False,
+    )
     return client
 
 
@@ -107,8 +115,11 @@ def index_file(file, index_name):
         if 'productId' not in doc or len(doc['productId']) == 0:
             continue
         #### Step 2.b: Create a valid OpenSearch Doc and bulk index 2000 docs at a time
-        the_doc = None
-        docs.append(the_doc)
+        doc['_index'] = index_name
+        doc['_id'] = doc['sku'][0]
+        docs_indexed += 1
+        docs.append(doc)
+
 
     return docs_indexed
 
